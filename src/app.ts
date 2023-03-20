f2a0a46c2e4242cd09a26884732ddcdd07e89632
shahreazebd <shahreaz@evidentbd.com>
2023-03-21 00:47:15 +0600

    Add logger and env

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from "express"
+import dotenv from "dotenv"
+import logger from "./utils/logger"
+import morgan from "morgan"
 
+dotenv.config()
+
+// express config
 const app = express()
+app.use(morgan("tiny"))
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+  res.send({ message: "OK" })
+})
+
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log("http://localhost:3000")
+  logger.info(`http://localhost:${process.env.PORT}`)
 })
