65639fb2ab2c88531ca98d863aba8514cb8de95b
shahreazebd <shahreaz@evidentbd.com>
2023-03-23 02:04:19 +0600

    create user route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,27 @@
 import type { Request, Response } from "express"
 import { CreateUserInput } from "../schemas/user.schema"
+import logger from "../utils/logger"
+import mongoose from "mongoose"
+import { createUser } from "../services/user.service"
+import { number } from "zod"
 
-export function createUserHandler(
+export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput>,
   res: Response
 ) {
-  const body = req.body
+  try {
+    const user = await createUser(req.body)
 
-  res.send(body)
+    res.send({
+      message: "success",
+      data: user,
+    })
+  } catch (error: any) {
+    logger.warn(error)
+
+    if (error.code === 11000)
+      return res.status(409).send({ message: "Account already exists" })
+
+    res.status(500).send(error.message)
+  }
 }
